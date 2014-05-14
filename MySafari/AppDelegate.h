9324a9085c05aeb2dc5ad0fbd0ae0a50d0f20e1a
Abdullah AlMubarak <abdullah.almubarak@ymail.com>
2014-05-14 13:25:10 -0500

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Abdullah AlMubarak on 5/14/14.
+//  Copyright (c) 2014 Abdullah AlMubarak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MySafari/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MySafari
+//
+//  Created by Abdullah AlMubarak on 5/14/14.
+//  Copyright (c) 2014 Abdullah AlMubarak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
